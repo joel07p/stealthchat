143641d636378d4be22f0352efeaca00ad42c4e4
tobiasmi <mikatobi12@gmail.com>
2024-03-18 11:53:45 +0100

    Common Folder added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserService } from './user/user.service';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [
@@ -12,7 +12,7 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     ConfigModule.forRoot({
       envFilePath: "./../.env"
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, UserService],
