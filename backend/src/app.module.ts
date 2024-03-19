3f37976dd0f6d4e8894a99172c95b1a68bb10e92
tobiasmi <mikatobi12@gmail.com>
2024-03-19 19:58:12 +0100

    ffff

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './modules/user/user.module';
-import { InvitationModule } from './modules/invitation/invitation.module';
 import { GroupModule } from './modules/group/group.module';
-import { RoomModule } from './modules/room/room.module';
+import { InvitationModule } from './modules/invitation/invitation.module';
 import { MessageModule } from './modules/message/message.module';
 import { PermissionModule } from './modules/permission/permission.module';
+import { RoomModule } from './modules/room/room.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
