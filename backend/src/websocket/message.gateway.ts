f2b72928ecab512d73e54bbb3742d19be58bbe64
tobiasmi <mikatobi12@gmail.com>
2024-04-30 21:22:48 +0200

    OnGatewayInit method implented

--- backend/src/websocket/message.gateway.ts
+++ backend/src/websocket/message.gateway.ts
@@ -1,9 +1,16 @@
-import { WebSocketGateway } from "@nestjs/websockets";
+import { Logger } from "@nestjs/common";
+import { OnGatewayInit, WebSocketGateway } from "@nestjs/websockets";
 import { MessageService } from "src/modules/message/message.service";
 
 @WebSocketGateway()
-export class MessageGateway {
+export class MessageGateway implements OnGatewayInit {
+    private readonly logger = new Logger(MessageGateway.name)
+    
     constructor(
         private readonly messageService: MessageService, 
     ) {}
+
+    afterInit(): void {
+        this.logger.log("Websocket Gateway initialized")
+    }
 }
